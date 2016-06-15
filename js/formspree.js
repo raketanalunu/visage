9e4696dc5daa4a18418ee282fe3e9ed32052b953
raketanalunu <kanevanalune@gmail.com>
2016-06-15 19:00:41 +0300

    mail

--- js/formspree.js
+++ js/formspree.js
@@ -1,4 +1,4 @@
 var a = 'forms' + 'pree.' + 'io/';
-var b = 'nalune' + '@gmail' + '.com';
+var b = '199' + '@yandex' + '.ru';
 var contactform =  document.getElementById("contactform");
-contactform.setAttribute("action",  "https://" + a + "kaneva" + b);
+contactform.setAttribute("action",  "https://" + a + "Jessy" + b);
